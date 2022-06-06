1130360a59e19a6f25098478d9f5201093ee53a2
Laura Stasiulė <laura.stasiule@nortal.com>
2022-06-06 09:29:11 +0300

    Initial commit

---
+++ src/test/java/learncucumber/MyStepdefs.java
@@ -0,0 +1,26 @@
+package learncucumber;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+
+    @Given("I'm dummy")
+    public void iMDummy() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I do dummy things")
+    public void iDoDummyThings() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("dummy things happen")
+    public void dummyThingsHappen() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
